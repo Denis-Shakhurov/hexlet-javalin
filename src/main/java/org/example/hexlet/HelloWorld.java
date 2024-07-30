e59146ec25ea8fc41f0ec3cf2c5febfa11d159db
DenisShakhurov <disa.sdf86@gmail.com>
2024-07-30 19:15:02 +0700

    update HelloWorld.java

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -5,11 +5,15 @@ import io.javalin.Javalin;
 public class HelloWorld {
     public static void main(String[] args) {
         // Создаем приложение
-        var app = Javalin.create(config -> {
-            config.bundledPlugins.enableDevLogging();
-        });
-        // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
+        Javalin app = getApp();
         app.start(7070); // Стартуем веб-сервер
+    }
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {config.bundledPlugins.enableDevLogging();});
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result("Hello, " + name + "!");
+        });
+        return app;
     }
 }
